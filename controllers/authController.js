0034ac038ee368777c162098a413a0faa0aa18d2
ertugrulocal17 <ertugrulocal17@gmail.com>
2022-03-16 00:39:52 +0300

    added user roles and auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,33 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
 
 exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
 
-    res.status(201).json({
-      status: 'success',
-      user,
+    res.status(201).redirect('/login');
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+
+exports.loginUser = (req, res) => {
+  try {
+    const { email, password } = req.body;
+    User.findOne({ email }, (err, user) => {
+      if (user) {
+        bcrypt.compare(password, user.password, (err, same) => {
+          if (same) {
+            // USER SESSION
+            req.session.userID = user._id;
+            res.status(200).redirect('/users/dashboard');
+          }
+        });
+      }
     });
   } catch (error) {
     res.status(400).json({
@@ -14,4 +35,18 @@ exports.createUser = async (req, res) => {
       error,
     });
   }
+};
+
+exports.logoutUser = (req, res) => {
+  req.session.destroy(() => {
+    res.redirect('/');
+  });
+};
+
+exports.getDashboardPage = async (req, res) => {
+  const user = await User.findOne({ _id: req.session.userID });
+  res.status(200).render('dashboard', {
+    page_name: 'dashboard',
+    user,
+  });
 };
